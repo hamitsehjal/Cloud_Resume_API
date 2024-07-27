64e418a91ac3ad0f51efaee50e50bf1f64362195
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-07-26 22:00:45 -0400

    add support for theme

--- README.md
+++ README.md
@@ -1,18 +1,44 @@
 # Cloud Resume API with Azure
 
-[![Deploy to Azure Functions](https://github.com/rishabkumar7/aws-resume-api/actions/workflows/main.yml/badge.svg)](https://github.com/hamitsehjal/Serverless-Cloud-Resume/actions/workflows/function.yml)
+![Deploy to Azure Functions](https://github.com/hamitsehjal/Serverless-Cloud-Resume/actions/workflows/function.yml/badge.svg)
 
-A Serverless Cloud Resume API with Azure.
+A Serverless Cloud Resume API with Azure, supporting both JSON and HTML formats.
 
 ## Demo
 
-`curl https://zw2tkpprcyswk7hm7nqrmugqli0krcrq.lambda-url.ca-central-1.on.aws/`
+### JSON Format (Default)
+
+```
+curl https://resume26hamitfunc.azurewebsites.net/
+```
+
+### HTML Format
+
+```
+curl https://resume26hamitfunc.azurewebsites.net/hamitsehjal
+```
+
+*For better results with the HTML version, copy the URL and paste it directly into your web browser.*
+
+## Features
+
+- Serves resume data in two formats:
+    1. JSON: Available at the root URL
+    2. HTML: Available at the `/hamitsehjal` route
+- Uses Azure Functions for serverless architecture
+- Stores data in CosmosDB for scalability and performance
+- Infrastructure as Code using Terraform
 
 ## Tech-stack
 
 - Azure Functions
 - CosmosDB
 - Terraform
+
+## Usage
+
+- To retrieve the resume in JSON format, send a GET request to the root URL.
+- To retrieve the resume in HTML format, send a GET request to the `/hamitsehjal` route.
 
 ## Author
 
--- src/functions/resume.js
+++ src/functions/resume.js
@@ -1,40 +1,59 @@
-const { app, input } = require("@azure/functions");
-const { render } = require("jsonresume-theme-elegant");
+const {app, input} = require("@azure/functions");
+const {render} = require("jsonresume-theme-elegant");
 
 const cosmosInput = input.cosmosDB({
-  databaseName: "resume-db",
-  containerName: "resume-container",
-  connection: "CosmosDBConnectionString",
+    databaseName: "resume-db",
+    containerName: "resume-container",
+    connection: "CosmosDBConnectionString",
 });
 
 app.http("resume", {
-  methods: ["GET", "POST"],
-  authLevel: "anonymous",
-  route: "hamitsehjal",
-  extraInputs: [cosmosInput],
-  handler: async (request, context) => {
-    context.log(`Http function processed request for url "${request.url}"`);
-    const documents = context.extraInputs.get(cosmosInput);
-    if (documents && documents.length > 0) {
-      const { id, _rid, _self, _etag, _attachments, _ts, ...cleanedDocument } =
-        documents[0];
+    methods: ["GET", "POST"],
+    authLevel: "anonymous",
+    route: "{*route}",
+    extraInputs: [cosmosInput],
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        const documents = context.extraInputs.get(cosmosInput);
+        if (!documents || documents.length === 0) {
+            return {
+                statusCode: 404,
+                body: "No Resume Found"
+            }
+        }
 
-      try {
-        const styledResume = render(cleanedDocument);
-        return {
-          statusCode: 200,
-          headers: {
-            "content-type": "text/html",
-          },
-          body: styledResume,
-        };
-      } catch (error) {
-        return {
-          body: JSON.stringify(cleanedDocument, null, 2),
-        };
-      }
+        // Removing cosmosDB metadata
+        const {id, _rid, _self, _etag, _attachments, _ts, ...cleanedDocument} =
+            documents[0];
+
+        // Extracting route from the request
+        const route = request.params.route || ""
+
+        if (route === "hamitsehjal") {
+            // Serve HTML Version
+            try {
+                const styledResume = render(cleanedDocument);
+                return {
+                    statusCode: 200,
+                    headers: {
+                        "content-type": "text/html",
+                    },
+                    body: styledResume,
+
+                };
+            } catch (error) {
+                return {
+                    body: JSON.stringify(cleanedDocument, null, 2),
+                };
+            }
+        } else {
+            // server JSON version
+            return {
+                body: JSON.stringify(cleanedDocument, null, 2),
+            };
+        }
+
     }
 
-    return { body: `Resume not Found!!`, statusCode: 404 };
-  },
+
 });
